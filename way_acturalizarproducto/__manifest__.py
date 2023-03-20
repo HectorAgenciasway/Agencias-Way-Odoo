06f851b8aca4eb13e574fb237957288412f60912
HectorAgenciasway <123115384+HectorAgenciasway@users.noreply.github.com>
2023-03-20 14:42:29 -0600

    boton_actualizarproducto_1.1.0
    primera prueba de boton actualizar producto

---
+++ way_acturalizarproducto/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Módulo actualizarproducto',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': "Módulo ActualizarProducto AW",
+    'author': 'Abner Alvarado, Hector Contreras, Gustavo Alvarado',
+    'website': '',
+    'depends': ['base', 'stock'],
+    'data': ['views/product_view.xml',],
+    'installable': True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
