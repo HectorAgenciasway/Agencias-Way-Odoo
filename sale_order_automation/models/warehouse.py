759120d25939f1622b7b4c0bbde01dae112e3d74
HectorAgenciasway <123115384+HectorAgenciasway@users.noreply.github.com>
2023-04-03 07:30:54 -0600

    prueba1.1.0.2

--- sale_order_automation/models/warehouse.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class StockWarehouse(models.Model):
-    _inherit = "stock.warehouse"
-
-    x_is_delivery_set_to_done = fields.Boolean(string="Is Delivery Set to Done")
-    x_create_invoice=fields.Boolean(string='Create Invoice?')
-    x_validate_invoice = fields.Boolean(string='Validate invoice?')
